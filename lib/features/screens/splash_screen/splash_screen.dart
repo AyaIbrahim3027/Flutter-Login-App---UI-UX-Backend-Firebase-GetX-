236c56944a884f296b47b770f6147b4214a3596c
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-24 14:03:42 +0200

    create Animate any widget

--- lib/features/screens/splash_screen/splash_screen.dart
+++ lib/features/screens/splash_screen/splash_screen.dart
@@ -1,82 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:login/features/controllers/splash_screen_controller.dart';
+import 'package:login/fade_in_animation/fade_in_animation_controller.dart';
+import 'package:login/fade_in_animation/fade_in_animation_model.dart';
+
+import '../../../fade_in_animation/animation_design.dart';
 
 class SplashScreen extends StatelessWidget {
   SplashScreen({super.key});
 
-  final splashScreenController = Get.put(SplashScreenController());
   @override
   Widget build(BuildContext context) {
-    splashScreenController.startAnimation();
+    final controller = Get.put(FadeInAnimationController());
+
+    controller.startSplashAnimation();
+
     return Scaffold(
       body: Stack(
         children: [
-          Obx(() => AnimatedPositioned(
-              duration: const Duration(milliseconds: 1600),
-              top: splashScreenController.animate.value ? 0 : -30,
-              left: splashScreenController.animate.value ? 0 : -30,
-              child: Container(
-                width: 100,
-                height: 110,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.indigo,
-                ),
+          TFadeInAnimation(
+            durationInMs: 1600,
+            animate: TAnimatePosition(
+              topAfter: 0,
+              topBefore: -30,
+              leftBefore: -30,
+              leftAfter: 0,
+            ),
+            child: Container(
+              width: 100,
+              height: 110,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.indigo,
               ),
             ),
           ),
-          Obx(() => AnimatedPositioned(
-              duration: const Duration(milliseconds: 1600),
-              top: 100,
-              left: splashScreenController.animate.value ? 40 : -40,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 1600),
-                opacity: splashScreenController.animate.value ? 1 : 0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Learn To Code',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    Text(
-                      'Free For Everyone',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                  ],
+          TFadeInAnimation(
+            durationInMs: 2000,
+            animate: TAnimatePosition(
+              topBefore: 100,
+              topAfter: 100,
+              leftAfter: 40,
+              leftBefore: -40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Learn To Code',
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
-              ),
+                Text(
+                  'Free For Everyone',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
             ),
           ),
-          Obx(() => AnimatedPositioned(
-              duration: const Duration(milliseconds: 2400),
-              bottom: splashScreenController.animate.value ? 130 : -130,
-              left: splashScreenController.animate.value ? -45 : 45,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 2000),
-                opacity: splashScreenController.animate.value ? 1 : 0,
-                child: const Image(
-                  image: AssetImage('assets/hacker.png'),
-                ),
-              ),
+          TFadeInAnimation(
+            durationInMs: 2400,
+            animate: TAnimatePosition(
+              bottomBefore: -130,
+              bottomAfter: 130,
+              leftAfter: -45,
+              leftBefore: 45,
+            ),
+            child: const Image(
+              image: AssetImage('assets/hacker.png'),
             ),
           ),
-          Obx(() => AnimatedPositioned(
-              duration: const Duration(milliseconds: 2400),
-              bottom: splashScreenController.animate.value ? 40 : -40,
-              right: splashScreenController.animate.value ? 20 : -20,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 2000),
-                opacity: splashScreenController.animate.value ? 1 : 0,
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.indigo,
-                  ),
-                ),
+          TFadeInAnimation(
+            durationInMs: 2400,
+            animate: TAnimatePosition(
+              bottomBefore: -40,
+              bottomAfter: 40,
+              rightAfter: -20,
+              rightBefore: 20,
+            ),
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.indigo,
               ),
             ),
           ),
--- lib/features/screens/welcome/welcome_screen.dart
+++ lib/features/screens/welcome/welcome_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login/fade_in_animation/animation_design.dart';
+import 'package:login/fade_in_animation/fade_in_animation_model.dart';
+
+import '../../../fade_in_animation/fade_in_animation_controller.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
+   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(FadeInAnimationController());
+    controller.startAnimation();
     var mediaQuery = MediaQuery.of(context);
     var height = mediaQuery.size.height;
     var brightness = mediaQuery.platformBrightness;
@@ -13,71 +20,88 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor:
           isDarkMode ? const Color(0xff272727) : const Color(0xFFFFE082),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image(
-              image: const AssetImage('assets/welcome.png'),
-              height: height * 0.5,
+      body: Stack(
+        children: [
+          TFadeInAnimation(
+            durationInMs: 1200,
+            animate: TAnimatePosition(
+              bottomBefore: -100,
+              bottomAfter: 0,
+              leftBefore: 0,
+              leftAfter: 0,
+              rightAfter: 0,
+              rightBefore: 0,
+              topAfter: 0,
+              topBefore: 0,
             ),
-            Column(
-              children: [
-                const Text(
-                  'Building Awesome Apps',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(
+                    image: const AssetImage('assets/welcome.png'),
+                    height: height * 0.5,
                   ),
-                ),
-                Text(
-                  "Let's start your journey with us on this "
-                  "amazing and easy platform",
-                  style: Theme.of(context).textTheme.titleMedium,
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(),
-                      foregroundColor: const Color(0xff272727),
-                      side: const BorderSide(
-                        color: Color(0xff272727),
+                  Column(
+                    children: [
+                      const Text(
+                        'Building Awesome Apps',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                    ),
-                    child: Text('Login'.toUpperCase()),
-                  ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      shape: const RoundedRectangleBorder(),
-                      foregroundColor: Colors.white,
-                      backgroundColor: const Color(0xff272727),
-                      side: const BorderSide(
-                        color: Color(0xff272727),
+                      Text(
+                        "Let's start your journey with us on this "
+                        "amazing and easy platform",
+                        style: Theme.of(context).textTheme.titleMedium,
+                        textAlign: TextAlign.center,
                       ),
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                    ),
-                    child: Text('Sign Up'.toUpperCase()),
+                    ],
                   ),
-                ),
-              ],
+                  Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(),
+                            foregroundColor: const Color(0xff272727),
+                            side: const BorderSide(
+                              color: Color(0xff272727),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                          ),
+                          child: Text('Login'.toUpperCase()),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            shape: const RoundedRectangleBorder(),
+                            foregroundColor: Colors.white,
+                            backgroundColor: const Color(0xff272727),
+                            side: const BorderSide(
+                              color: Color(0xff272727),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                          ),
+                          child: Text('Sign Up'.toUpperCase()),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
